b7be6d40ca0e8f30b0898988e475a2a35e155604
kevinInsight <958330481@qq.com>
2018-05-01 09:49:19 +0800

    目录调整

---
+++ pages/voterDetail/voterDetail.js
@@ -0,0 +1,56 @@
+const util = require('../../utils/util.js')
+Page({
+    data: {
+        voteDetails: '',
+        voteId: '',
+        voteTitle: ''
+    },
+    onLoad: function(option) {
+        let self = this;
+        let authorization = wx.getStorageSync('authorization');
+        let voteId;
+        if (option) {
+            voteId = option.voteId
+        } else {
+            voteId = self.data.voteId
+        }
+        if (voteId) {
+            self.setData({
+                voteId: voteId
+            })
+        }
+        wx.request({
+            url: util.baseUrl + '/api/votes/' + voteId + '/voters',
+            method: 'GET',
+            header: {
+                'content-type': 'application/json',
+                'accept': 'application/json',
+                Authorization: authorization
+            },
+            success: function(res) {
+                if (res.statusCode === 200) {
+                    self.setData({
+                        voteDetails: res.data.data,
+                        voteTitle: res.data.data.title
+                    })
+                    wx.stopPullDownRefresh()
+                    wx.hideNavigationBarLoading()
+                }
+            },
+            fail: function(res) {
+                console.log(res)
+                wx.showToast({
+                    title: '数据请求失败，请稍后再试',
+                    icon: 'none',
+                    duration: 1500,
+                    mask: true
+                })
+            }
+        })
+    },
+    onPullDownRefresh: function() {
+        let self = this;
+        wx.showNavigationBarLoading()
+        self.onLoad();
+    }
+})
