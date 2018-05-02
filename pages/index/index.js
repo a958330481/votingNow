16038ae7e13e825939abaabbe49f7455c5a7deee
kevinInsight <958330481@qq.com>
2018-05-02 17:59:05 +0800

    优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -67,7 +67,7 @@ Page({
     },
     targetToAdd: function() {
         wx.navigateTo({
-            url: '../comment/comment',
+            url: '/pages/comment/comment',
             success: function(res) {
                 console.log(res)
             }
@@ -152,7 +152,7 @@ Page({
                     if (type === "refresh") {
                         wx.showToast({
                             title: '刷新成功',
-                            icon: 'success', // loading
+                            icon: 'success',
                             duration: 1200,
                             mask: true
                         });
